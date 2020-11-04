a13c3283ae6f76e47b465ae75b1ba96719ca23c8
Khoovi Seewoonauth <khoovirajsingh.seewoonauth@codurance.com>
2020-11-04 15:17:24 +0000

    Write bash script so as to download docker images

---
+++ migrate-to-azure-docker-registry.sh
@@ -0,0 +1,26 @@
+#script input param is acr registry url, glasswall registry username, password
+#
+#for each helm directory
+#  run helm template . and grep image repos
+#  for each image repo
+#    if docker registry is glasswallsolutions then we have to do docker login
+#    run docker pull image repo
+#    run docker tag to new acr registry
+#    run docker push to new acr registry
+
+
+#!/bin/bash
+shopt -s dotglob
+find * -prune -type d | while IFS= read -r d; do
+    echo "$d"
+    cd $d
+
+    helm template . | grep -i "image:" | while read -r line; do
+      sed -e "s/image: //" -e "s/baseImage: //" | while read -r line; do
+        echo "Image url is $line"
+        docker pull $line;
+      done
+    done
+
+    cd ..
+done

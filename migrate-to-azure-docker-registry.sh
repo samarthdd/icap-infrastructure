53c5d7484acb72ea1b9026c78c0278a9572b3b91
Sylvester Loreto <sylvester.loreto@codurance.com>
2020-11-04 16:15:27 +0000

    At this stage we're able to download most images

--- migrate-to-azure-docker-registry.sh
+++ migrate-to-azure-docker-registry.sh
@@ -17,8 +17,12 @@ find * -prune -type d | while IFS= read -r d; do
 
     helm template . | grep -i "image:" | while read -r line; do
       sed -e "s/image: //" -e "s/baseImage: //" | while read -r line; do
-        echo "Docker registry url is $line"
+        while [[ $line == *'"'* ]]; do
+          line=${line#*'"'}; line=${line%'"'*};
+        done
+        echo "\n\nDocker registry url is $line"
         docker pull $line;
+        echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n\n"
       done
     done
 

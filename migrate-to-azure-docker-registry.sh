f0b83ecb031b114d2d31eaaafece559f958274e3
Sylvester Loreto <sylvester.loreto@codurance.com>
2020-11-04 17:16:39 +0000

    At this stage we're able to download most images and push them to ACR

--- migrate-to-azure-docker-registry.sh
+++ migrate-to-azure-docker-registry.sh
@@ -65,10 +65,19 @@ done
 #    run docker pull image repo
 #    run docker tag to new acr registry
 #    run docker push to new acr registry
-
-
+=======
 #!/bin/bash
+
+
+loginToAcr () {
+  accessToken=$(az acr login --name gwicapcontainerregistry --expose-token | jq -r '.accessToken')
+  docker login gwicapcontainerregistry.azurecr.io --username 00000000-0000-0000-0000-000000000000	--password "$accessToken"
+}
+
+loginToAcr
+
 shopt -s dotglob
+
 find * -prune -type d | while IFS= read -r d; do
     echo "$d"
     cd $d
@@ -78,8 +87,11 @@ find * -prune -type d | while IFS= read -r d; do
         while [[ $line == *'"'* ]]; do
           line=${line#*'"'}; line=${line%'"'*};
         done
+
         echo "\n\nDocker registry url is $line"
         docker pull $line;
+        docker tag $line gwicapcontainerregistry.azurecr.io/$line
+        docker push gwicapcontainerregistry.azurecr.io/$line
         echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n\n"
       done
     done
